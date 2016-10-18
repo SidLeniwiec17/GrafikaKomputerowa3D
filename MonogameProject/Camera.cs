23d35b1cf2b61b2ff23ec480128f5bdd51c18fc2
SidLeniwiec <piotrmichalik17@wp.pl>
2016-10-18 20:56:34 +0200

    pierwszy commit

---
+++ MonogameProject/Camera.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonogameProject
+{
+    class Camera
+    {
+        // We need this to calculate the aspectRatio
+        // in the ProjectionMatrix property.
+        GraphicsDevice graphicsDevice;
+
+        Vector3 position = new Vector3(0, 50, 10);
+
+        float angleZ, angleX;
+
+        public Matrix ViewMatrix
+        {
+            get
+            {   var lookAtVector = new Vector3(0, -1, -0.1f);
+
+                var rotationMatrix = Matrix.CreateRotationZ(angleZ);
+                var rotationMatrix2 = Matrix.CreateRotationX(angleX);
+                lookAtVector = Vector3.Transform(lookAtVector, rotationMatrix);
+                lookAtVector = Vector3.Transform(lookAtVector, rotationMatrix2);
+                lookAtVector += position;
+
+                var upVector = Vector3.UnitZ;
+
+                return Matrix.CreateLookAt(
+                    position, lookAtVector, upVector);
+            }
+        }
+
+        public Matrix ProjectionMatrix
+        {
+            get
+            {
+                float fieldOfView = Microsoft.Xna.Framework.MathHelper.PiOver4;
+                float nearClipPlane = 1;
+                float farClipPlane = 200;
+                float aspectRatio = graphicsDevice.Viewport.Width / (float)graphicsDevice.Viewport.Height;
+
+                return Matrix.CreatePerspectiveFieldOfView(
+                    fieldOfView, aspectRatio, nearClipPlane, farClipPlane);
+            }
+        }
+
+        public Camera(GraphicsDevice graphicsDevice)
+        {
+            this.graphicsDevice = graphicsDevice;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            ////---------------------LEWO PRAWO GORA DOL
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                position.X += 0.5f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                position.X -= 0.5f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                position.Y -= 0.5f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                position.Y += 0.5f;
+            }
+            ////----------------------PRZOD TYL
+            if (Keyboard.GetState().IsKeyDown(Keys.OemPlus))
+            {
+                position.Z += 0.5f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.OemMinus))
+            {
+                position.Z -= 0.5f;
+            }
+
+            ////--------------------OBROTY LEWO PRAWO GORA DOL
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                angleX -= 0.02f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                angleX += 0.02f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                angleZ += 0.02f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                angleZ -= 0.02f;
+            }
+        }
+    }
+}
---
+++ MonogameProject/Game1.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonogameProject
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+
+        VertexPosition[] stationVerts;
+
+        BasicEffect effect;
+
+        Camera camera;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.IsFullScreen = true;
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// </summary>
+        protected override void Initialize()
+        {
+            stationVerts = InitializeStation();
+
+            effect = new BasicEffect(graphics.GraphicsDevice);
+
+            camera = new Camera(graphics.GraphicsDevice);
+
+            base.Initialize();
+        }
+
+        public VertexPosition[] InitializeStation()
+        {
+            VertexPosition[] sVerts = new VertexPosition[6];
+            sVerts[0].Position = new Vector3(-20, -20, 0);
+            sVerts[1].Position = new Vector3(-20, 20, 0);
+            sVerts[2].Position = new Vector3(20, -20, 0);
+            sVerts[3].Position = sVerts[1].Position;
+            sVerts[4].Position = new Vector3(20, 20, 0);
+            sVerts[5].Position = sVerts[2].Position;
+
+            return sVerts;
+        }
+       
+        /// <summary>
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            //spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// </summary>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            camera.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// </summary>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            DrawStation();
+
+            base.Draw(gameTime);
+        }
+
+        void DrawStation()
+        {
+            effect.View = camera.ViewMatrix;
+            effect.Projection = camera.ProjectionMatrix;
+
+            effect.TextureEnabled = false;
+
+            foreach (var pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+
+                graphics.GraphicsDevice.DrawUserPrimitives(
+                    PrimitiveType.TriangleList, stationVerts, 0, 2); // The number of triangles to draw
+            }
+        }
+    }
+}

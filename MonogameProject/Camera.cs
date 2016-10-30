d44b671eab8999b8b1c0344767480ccc3a977765
SidLeniwiec <piotrmichalik17@wp.pl>
2016-10-30 22:24:43 +0100

    kamera z tutoriala

--- MonogameProject/Camera.cs
+++ MonogameProject/Camera.cs
@@ -15,29 +15,37 @@ namespace MonogameProject
         // in the ProjectionMatrix property.
         GraphicsDevice graphicsDevice;
 
-        Vector3 position = new Vector3(0, -50, 10);
-        
-        float angleZ, angleX;
-        
-        public Matrix ViewMatrix
-        {
-            get
-            {   
-                var lookAtVector = new Vector3(0, 1, -0.1f);
+        /* Vector3 position = new Vector3(0, -50, 10);
+        float angleZ, angleX;*/
 
-                var rotationMatrix = Matrix.CreateRotationZ(angleZ);
-                var rotationMatrix2 = Matrix.CreateRotationX(angleX);
-                lookAtVector = Vector3.Transform(lookAtVector, rotationMatrix);
-                lookAtVector = Vector3.Transform(lookAtVector, rotationMatrix2);
-                lookAtVector += position;
+        //------------------------------------------------------------------------
+        Vector3 cameraPosition = new Vector3(0, 0, 0);
+        float leftrightRot = MathHelper.PiOver2;
+        float updownRot = -MathHelper.Pi / 10.0f;
+        const float rotationSpeed = 0.3f;
+        const float moveSpeed = 1.0f;
+        public Matrix ViewMatrix;
+        //------------------------------------------------------------------------
 
-                var upVector = Vector3.UnitZ;
+        /* public Matrix ViewMatrix
+         {
+             get
+             {   
+                 var lookAtVector = new Vector3(0, 1, -0.1f);
 
-                return Matrix.CreateLookAt(
-                    position, lookAtVector, upVector);
-            }
-        }
+                 var rotationMatrix = Matrix.CreateRotationZ(angleZ);
+                 var rotationMatrix2 = Matrix.CreateRotationX(angleX);
+                 lookAtVector = Vector3.Transform(lookAtVector, rotationMatrix);
+                 lookAtVector = Vector3.Transform(lookAtVector, rotationMatrix2);
+                 lookAtVector += position;
 
+                 var upVector = Vector3.UnitZ;
+
+                 return Matrix.CreateLookAt(
+                     position, lookAtVector, upVector);
+             }
+         }
+         */
         public Matrix ProjectionMatrix
         {
             get
@@ -59,58 +67,93 @@ namespace MonogameProject
 
         public void Update(GameTime gameTime)
         {
+            Vector3 moveVector = new Vector3(0, 0, 0);
+
             ////---------------------LEWO PRAWO GORA DOL
             if (Keyboard.GetState().IsKeyDown(Keys.Left))
             {
-                position.X -= 1.0f;
+                moveVector += new Vector3(-1, 0, 0);
+                /*position.X -= 1.0f;
                 position.Y -= (float)Math.Sin((double)angleZ);
-                position.Z += (float)Math.Sin((double)angleX);
+                position.Z += (float)Math.Sin((double)angleX);*/
             }
             if (Keyboard.GetState().IsKeyDown(Keys.Right))
             {
-                position.X += 1.0f;
+                moveVector += new Vector3(1, 0, 0);
+                /*position.X += 1.0f;
                 position.Y += (float)Math.Sin((double)angleZ);
-                position.Z -= (float)Math.Sin((double)angleX);
+                position.Z -= (float)Math.Sin((double)angleX);*/
             }
             if (Keyboard.GetState().IsKeyDown(Keys.Up))
             {
-                position.Y += 1.0f;
+                moveVector += new Vector3(0, 0, -1);
+                /*position.Y += 1.0f;
                 position.X -= (float)Math.Sin((double)angleZ);
-                position.Z += (float)Math.Sin((double)angleX);
+                position.Z += (float)Math.Sin((double)angleX);*/
             }
             if (Keyboard.GetState().IsKeyDown(Keys.Down))
             {
-                position.Y -= 1.0f;
+                moveVector += new Vector3(0, 0, 1);
+                /*position.Y -= 1.0f;
                 position.X += (float)Math.Sin((double)angleZ);
-                position.Z -= (float)Math.Sin((double)angleX);
+                position.Z -= (float)Math.Sin((double)angleX);*/
             }
             ////----------------------PRZOD TYL
             if (Keyboard.GetState().IsKeyDown(Keys.OemPlus))
             {
-                position.Z += 0.5f;
+                moveVector += new Vector3(0, 1, 0);
+                //position.Z += 0.5f;
             }
             if (Keyboard.GetState().IsKeyDown(Keys.OemMinus))
             {
-                position.Z -= 0.5f;
+
+                moveVector += new Vector3(0, -1, 0);   
+                //position.Z -= 0.5f;
             }
 
             ////--------------------OBROTY LEWO PRAWO GORA DOL
             if (Keyboard.GetState().IsKeyDown(Keys.W))
             {
-                angleX += 0.02f;
-                            }
+                updownRot += 0.02f;
+            }
             if (Keyboard.GetState().IsKeyDown(Keys.S))
             {
-                angleX -= 0.02f;
+                updownRot -= 0.02f;
             }
             if (Keyboard.GetState().IsKeyDown(Keys.A))
             {
-                angleZ += 0.02f;
+                leftrightRot += 0.02f;
             }
             if (Keyboard.GetState().IsKeyDown(Keys.D))
             {
-                angleZ -= 0.02f;
+                leftrightRot -= 0.02f;
             }
+
+            AddToCameraPosition(moveVector, gameTime);
         }
+
+
+        //------------------------------------------------------------------------
+        private void AddToCameraPosition(Vector3 vectorToAdd, GameTime gameTime)
+        {
+            Matrix cameraRotation = Matrix.CreateRotationX(updownRot) * Matrix.CreateRotationY(leftrightRot);
+            Vector3 rotatedVector = Vector3.Transform(vectorToAdd, cameraRotation);
+            cameraPosition += moveSpeed * rotatedVector;
+            UpdateViewMatrix();
+        }
+        private void UpdateViewMatrix()
+        {
+            Matrix cameraRotation = Matrix.CreateRotationX(updownRot) * Matrix.CreateRotationY(leftrightRot);
+
+            Vector3 cameraOriginalTarget = new Vector3(0, 0, -1);
+            Vector3 cameraRotatedTarget = Vector3.Transform(cameraOriginalTarget, cameraRotation);
+            Vector3 cameraFinalTarget = cameraPosition + cameraRotatedTarget;
+            
+            Vector3 cameraOriginalUpVector = new Vector3(0, 1, 0);
+            Vector3 cameraRotatedUpVector = Vector3.Transform(cameraOriginalUpVector, cameraRotation);
+
+            ViewMatrix = Matrix.CreateLookAt(cameraPosition, cameraFinalTarget, cameraRotatedUpVector);
+        }
+        //------------------------------------------------------------------------
     }
 }
